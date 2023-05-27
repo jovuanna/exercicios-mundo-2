216f45b9c43df49a08e462b50db5bfda410e4957
Giovanna Giselli Santana de Oliveira <113366850+jovuanna@users.noreply.github.com>
2023-05-27 18:30:27 -0300

    Add files via upload

---
+++ 045-pedra-papel-tesoura.py
@@ -0,0 +1,65 @@
+from random import randint
+from time import *
+print('''
+[ 0 ] PEDRA 
+[ 1 ] PAPEL
+[ 2 ] TESOURA
+''')
+computador = randint(0, 2)
+opcoes = int(input("Escolha uma das opções: "))
+
+print("-"*20)
+print("JO")
+sleep(0.5)
+print("KEN")
+sleep(0.5)
+print("PO!")
+sleep(0.5)
+
+if opcoes == 0:
+    print("-" * 20)
+    print("Jogador jogou Pedra.")
+    if computador == 0:
+        print("Computador jogou Pedra.")
+        print("-" * 20)
+        print("JOGADA EMPATADA")
+    elif computador == 1:
+        print("Computador jogou Papel.")
+        print("-" * 20)
+        print("JOGADOR VENCE")
+    else:
+        print("Computador jogou Tesoura.")
+        print("-" * 20)
+        print("COMPUTADOR VENCE")
+elif opcoes == 1:
+    print("-" * 20)
+    print("Jogador jogou Papel.")
+    if computador == 0:
+        print("Computador jogou Pedra.")
+        print("-" * 20)
+        print("JOGADOR VENCE")
+    elif computador == 1:
+        print("Computador jogou Papel.")
+        print("-" * 20)
+        print("JOGADA EMPATADA")
+    else:
+        print("Computador jogou Tesoura.")
+        print("-" * 20)
+        print("COMPUTADOR VENCE")
+elif opcoes == 2:
+    print("-" * 20)
+    print("Jogador jogou Tesoura.")
+    if computador == 0:
+        print("Computador jogou Pedra.")
+        print("-" * 20)
+        print("COMPUTADOR VENCE")
+    elif computador == 1:
+        print("Computador jogou Papel.")
+        print("-" * 20)
+        print("JOGADOR VENCE")
+    else:
+        print("Computador jogou Tesoura.")
+        print("-" * 20)
+        print("JOGADA EMPATADA")
+else:
+    print("Escolha uma opção válida.")
